0b8040879089b8f30c05cdcf977c6ff5f484883e
Richard Nienaber <rjnienaber@gmail.com>
2015-06-25 08:34:14 +0100

    small cleanup

--- code/rpc/server.rb
+++ code/rpc/server.rb
@@ -7,19 +7,19 @@ LOGGER = Logger.new(STDOUT)
 
 def execute_trade(sleep_time)
   sleep sleep_time
-  ('A'..'Z').to_a - ['A', 'E', 'I', 'O', 'U']
   "#{Time.now.strftime('%Y%m%d')}-#{(0..6).map { (('A'..'Z').to_a - ['A', 'E', 'I', 'O', 'U']).sample }.join}"
 end
 
-conn = MarchHare.connect(:automatically_recover => false, :user => "admin", :password => "Rabbit123")
+settings = {:user => "admin", :password => "Rabbit123", :port => 5673}
+LOGGER.info("SETTINGS: #{settings}")
+
+conn = MarchHare.connect(settings)
 ch = conn.create_channel
-# ch.prefetch = 1
-q = ch.queue('rpc.execute_trade')
+q = ch.queue('rpc.execute_trade', :durable => true, :auto_delete => false)
 x = ch.default_exchange
 executor = Executors.new_fixed_thread_pool(16)
 
-
-q.subscribe(:ack => true) do |delivery_info, payload|
+q.subscribe(:ack => true) do |meta_data, payload|
   executor.submit do
     # LOGGER.debug("STARTED: #{Thread.current.object_id}")
     begin
@@ -27,9 +27,9 @@ q.subscribe(:ack => true) do |delivery_info, payload|
       r = execute_trade(sleep_time)
       LOGGER.info("ID: #{r}, SLEEP: #{sleep_time}")
 
-      ch.ack(delivery_info.delivery_tag)
-      # LOGGER.info("PUBLISH: #{delivery_info.correlation_id}")
-      x.publish("#{r} - #{sleep_time}", :routing_key => delivery_info.reply_to)
+      ch.ack(meta_data.delivery_tag)
+      # LOGGER.info("PUBLISH: #{meta_data.correlation_id}")
+      x.publish("#{r} - #{sleep_time}", :routing_key => meta_data.reply_to)
     rescue => e
       LOGGER.error(e)
     end
